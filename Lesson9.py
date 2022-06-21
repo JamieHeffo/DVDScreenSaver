62c62aa119780eb4d95920ad2b47141b98d76faf
JamieHeffo <98745575+JamieHeffo@users.noreply.github.com>
2022-06-21 17:00:31 +0100

    Lesson 9

---
+++ Lesson9.py
@@ -0,0 +1,18 @@
+#Heres some strange stuff
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days : ", days, "Here are the months : ", months)
+print("Here are the months : ", months)
+
+print("""
+Theres something going on here.
+With the three double-quotes.
+We'll be able to tyoe as much as we like.
+Even 4 lines if we want 
+or 5
+or even 6
+
+I skipped a line
+""")
